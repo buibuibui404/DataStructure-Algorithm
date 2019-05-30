d2b068718130310590ac23691fb9e7eaf26d2971
buibuibui404 <44661702+buibuibui404@users.noreply.github.com>
2019-05-30 16:35:15 +1000

    add QuickSearch

---
+++ DivideConquer/QuickSearch.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+import javax.crypto.SealedObject;
+
+// given an unsorted list holding n numbers and a integer k, find the kth largest number
+
+public class QuickSearch{
+    public static int search(List<Integer> nums, int k){
+        if(nums.size() == 1){
+            return nums.get(0);
+        }
+        
+        int n = nums.get(0); // the random number
+        
+        List<Integer> left = new LinkedList<>();
+        List<Integer> equals = new LinkedList<>();
+        List<Integer> right = new LinkedList<>();  // LinkedList will have a better performance than ArrayList, since a lot of numbers may be added to the list.
+        
+        for(int i : nums){
+            if(i < n){
+                left.add(i);
+            }else if( i == n){
+                equals.add(i);
+            }else{
+                right.add(i);
+            }
+        }
+        
+        if(right.size() >= k){
+            return search( right ,k);
+        }else if(k <= right.size() + equals.size()){
+            return n;
+        }else{
+            return search( left ,k - right.size() - equals.size());
+        }
+        
+    }
+
+    public static void main(String[] args){
+        List<Integer> ls = new LinkedList<>();
+        ls.add(5);
+        ls.add(7);
+        ls.add(3);
+        ls.add(2);
+        ls.add(13);
+        ls.add(35);
+        ls.add(72);
+
+        // [5, 7, 3, 2, 13, 35, 72]
+
+        System.out.println(search(ls, 2));
+    }
+}
